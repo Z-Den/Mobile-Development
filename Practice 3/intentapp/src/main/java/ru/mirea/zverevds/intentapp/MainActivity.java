8c7463d573e674e6a9e2f5cdeb726c13777a1c57
Z-Den <gwnkjell@mail.ru>
2025-04-23 20:24:14 +0300

    intentApp ready

---
+++ Practice 3/intentapp/src/main/java/ru/mirea/zverevds/intentapp/MainActivity.java
@@ -0,0 +1,51 @@
+package ru.mirea.zverevds.intentapp;
+
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button = findViewById(R.id.button);
+        Intent intent = new Intent(this, SecondActivity.class);
+        long dateInMillis = System.currentTimeMillis();
+        String format = "yyyy-MM-dd HH:mm:ss";
+        final SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateString = sdf.format(new Date(dateInMillis));
+
+        View.OnClickListener oclNewActivity = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("date", dateString);
+                startActivity(intent);
+            }
+        };
+
+        button.setOnClickListener(oclNewActivity);
+
+
+    }
+}
